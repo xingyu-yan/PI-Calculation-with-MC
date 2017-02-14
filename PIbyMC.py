08505d56cd545ad6e5e2feb0cfc900c8d419750a
xingyu-yan <xingyu-yan@outlook.com>
2017-02-14 09:43:09 +0100

    Update PIbyMC.py

--- PIbyMC.py
+++ PIbyMC.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python3.5
+#!/usr/local/bin/python3.4
 # encoding: utf-8
 '''
 Created on December 5, 2016
@@ -24,23 +24,29 @@ N_in_circle = numpy.zeros(N_total)
 estimatePI =  numpy.zeros(N_total)
 
 for i in range(N_total):
-
+    
+    #Randomly initialize each point, with the central point (0,0) 
+    #and the range is [a, b]
     a = -1
     b = 1
     X = a + (b-a)*numpy.random.random(1)
-    Y = a + (b-a)*numpy.random.random(1)
+    Y = a + (b-a)*numpy.random.random(1)    
     
+    #Calculate the distance between the generated point and central point (0,0)
+    #If it is in the circle, noted 1; otherwise, noted 0
     DistenceN = X**2 + Y**2
     if DistenceN < 1:
         N_in_circle[i] = 1
     else:
         N_in_circle[i] = 0
     
+    #Calculate the estimation of PI
     estimatePI[i] = 4*sum(N_in_circle[:])/(i+1) # i is from 0 to (N_total-1)
 
 EstimatePI = 4*sum(N_in_circle)/N_total
 print('Estimation of PI is %f' % EstimatePI)
   
+#Plot the result  
 plt.figure(1)
 plt.plot(estimatePI)
 plt.title('N= %d ' ' Estimate of PI = %f' % (N_total,estimatePI[N_total-1]))
